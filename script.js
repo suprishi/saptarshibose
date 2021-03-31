4e6bc56e5acf12725bf526984314d8e29d293d1d
Saptarshi <saptarshi27@gmail.com>
2021-03-31 20:54:30 +0530

    added script js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* window.onscroll = function (e) {
+    e.preventDefault();
+    // print "false" if direction is down and "true" if up
+    if (this.oldScroll > this.scrollY) {
+        console.log('up');
+     } else {
+        window.scrollY = window.scrollY + window.innerHeight;
+        console.log('down');
+    }
+    // console.log(this.oldScroll > this.scrollY);
+    this.oldScroll = this.scrollY;
+} */
+console.log('Whatchu lookin for?');
